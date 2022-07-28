2ac455b99eb90a8ea4bde8756244918589ec9e5d
Usuario <andresroliveira98@gmail.com>
2022-07-28 19:58:36 -0300

    é isso aiiiii

--- T9.py
+++ T9.py
@@ -6,12 +6,8 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 
+VERBOSE = True
 
-#-----------------------------------
-def floter(x,Fx):
-    while len(x) >> 0:
-        Fx.append(float(x[0]))
-        x.pop(0)
 
 def cleanMT(origin, LM, LT):
     for x in origin:
@@ -19,35 +15,51 @@ def cleanMT(origin, LM, LT):
         a = a[:-2]
         b = ''.join(a)
         c = b.split('@')
-        LM.append(c[0])
-        LT.append(c[1])
+        LM.append(float(c[0]))
+        LT.append(float(c[1]))
+
 
 def clean(term):
     return re.sub("[^\d\.]", "", term)
 
-def sumtime(timeList):
+
+def sum_time(timeList):
     mysum = datetime.timedelta()
     for i in timeList:
         (h, m, s) = i.split(':')
         d = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
         mysum += d
-    return (str(mysum))
+    return str(mysum)
+
+
+def get_time():
+    return len(
+        pd.date_range("00:00:00", sum_time(duration_list),
+                      freq="1S").strftime('%H:%M:%S'))
+
 
 def points(time, temp):
-    time = len(pd.date_range("00:00:00", sumtime(duration_list), freq="1S").strftime('%H:%M:%S'))
-    for x in range(time):
+    time = get_time()
+    for _ in range(time):
         temperature_list.append(temp)
+
+
+########### main #############3
 
 #-----------------------------------
 #localiza os logs
 #mypath = "C:\\Users\\Usuario\AppData\Local\Programs\Editor - Programação\Saved Programs python\LabPetro"
-mypath = "C:\\Pycharm\Projeto - Labpetro"
+# mypath = "C:\\Pycharm\Projeto - Labpetro"
+
+mypath = '.'
 
 a = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+# print(a)
 b = []
 for item in a:
     if ".txt" in item:
         b.append(item)
+
 #-----------------------------------
 #sequencia de logs
 st = sorted(b)
@@ -58,15 +70,17 @@ temperature_list = []
 duration_list = []
 data_list = []
 hora_list = []
-while len(st) > 0:
+
+for st0 in st:
+
     #-----------------------------------
     #Abertura do log
-    with open(st[0]) as f:
+    with open(st0, encoding='iso-8859-1') as f:
         lines = f.readlines()
 
     #-----------------------------------
     #Pescar linha especifica
-    list_lines = (list(lines))
+    list_lines = list(lines)
 
     # -----------------------------------
     # Data e hora
@@ -74,7 +88,6 @@ while len(st) > 0:
     data_list.append(data)
     hora = clean(list_lines[10])[8:]
     hora_list.append(hora)
-
 
     # -----------------------------------
     # Duração esegundos convertida para minutos
@@ -84,19 +97,13 @@ while len(st) > 0:
     #-----------------------------------
     #remoção de cabeçalho
     del list_lines[0:41]
+
     #-----------------------------------
     #separa as colunas de Temperatura x Metro
-    list_T = []
-    list_M = []
-
-    cleanMT(list_lines,list_M,list_T)
-
-    #-----------------------------------
     FT = []
     FM = []
 
-    T = floter(list_T,FT )
-    M = floter(list_M,FM)
+    cleanMT(list_lines, FM, FT)
 
     #-----------------------------------
     #determina o range de espaço
@@ -115,48 +122,45 @@ while len(st) > 0:
             lista_sT.append(FT[y])
 
     temperaturas.append(np.mean(lista_sT))
-    #-----------------------------------
-    #elimna log usado
-    st.pop(0)
 
 #-----------------------------------
-print(temperaturas)
-print(duration_list)
-print(data_list)
-print(hora_list)
+if VERBOSE:
+    print(temperaturas)
+    print(duration_list)
+    print(data_list)
+    print(hora_list)
 #-----------------------------------
 
 #-----------------------------------
-elements = sumtime(duration_list)
+elements = sum_time(duration_list)
+
 #-----------------------------------
 #determina a largura da barra no grafico
 
-while len(temperaturas)>0:
-    points(duration_list[0],temperaturas[0])
-    duration_list.pop(0)
-    temperaturas.pop(0)
+for i in range(len(temperaturas)):
+    points(duration_list[i], temperaturas[i])
 
 #-----------------------------------
 #determina o eixo X
-rangeTime = pd.date_range("00:00:00", elements, periods=len(temperature_list)).strftime('%H:%M:%S')
-print('range', rangeTime)
+rangeTime = pd.date_range("00:00:00", elements,
+                          periods=len(temperature_list)).strftime('%H:%M:%S')
+if VERBOSE:
+    print('range', rangeTime)
 #-----------------------------------
 
-y=temperature_list
-x=rangeTime
+y = temperature_list
+x = rangeTime
 
 fig, ax = plt.subplots()
 fig.autofmt_xdate()
 
 ax.plot(x, y)
-ax.set(xlabel='Tempo', ylabel='Temperatura (°C)',
+ax.set(xlabel='Tempo',
+       ylabel='Temperatura (°C)',
        title='DTS - Temperatura por Tempo')
 
 start, end = ax.get_xlim()
-ax.xaxis.set_ticks(np.arange(start, end,40))
+ax.xaxis.set_ticks(np.arange(start, end, 40))
 
 ax.grid()
 plt.show()
-
-
-
